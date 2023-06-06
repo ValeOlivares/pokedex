c216d583b8f167450f1d4d5d9ebd4c1618c781ec
ValeOlivares <valentinaolivars85@gmail.com>
2023-06-06 19:27:44 -0400

    styles:add little style to pokedex header

--- src/components/pokedex/pokedex.tsx
+++ src/components/pokedex/pokedex.tsx
@@ -5,8 +5,10 @@ export default class Pokedex extends React.Component {
   render(){
     return (
       <div>
-         <section className="pokedex-header">Pokédex</section>
-         <div>Elemento de búsqueda</div>
+         <section className="pokedex-header">
+          <h1 className="pokedex-header-container">Pokédex</h1>
+         </section>
+         <section>Elemento de búsqueda</section>
          <div>Búsqueda avanzada</div>
          <div>Ordenar por</div>
          <div>pokemon</div>
